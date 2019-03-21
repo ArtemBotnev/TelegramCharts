e4c6a6a3c1c149a0c3911b5a7444b50b7308e99f
artem <arbotnev@yandex.ru>
2019-03-21 16:33:47 +0300

    added layout

--- app/src/main/java/ru/artembotnev/telegramcompetition/ui/MainActivity.kt
+++ app/src/main/java/ru/artembotnev/telegramcompetition/ui/MainActivity.kt
@@ -1,11 +1,10 @@
 package ru.artembotnev.telegramcompetition.ui
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.artembotnev.telegramcompetition.R
 import ru.artembotnev.telegramcompetition.model.entities.Chart
 import ru.artembotnev.telegramcompetition.readFile
@@ -14,25 +13,19 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val SOURCE_FILE_NAME = "chart_data.json"
-        private const val TAG = "MainActivity"
     }
 
     private lateinit var data: List<Chart>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        val view = ChartView(this).apply {
-            setBackgroundColor(Color.WHITE)
-        }
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
         val jsonString = assets.readFile(SOURCE_FILE_NAME)
-        Log.i(TAG, jsonString)
 
         data = GsonBuilder().create()
                 .fromJson(jsonString, object : TypeToken<List<Chart>>() {}.type)
 
-        view.chart = data[0]
+        chartView.chart = data[0]
     }
 }
